76970e1913037ac58166b5052af3e6565b5341c3
Bob Lannon <blannon@gmail.com>
2015-04-13 15:41:41 -0400

    updated intro notebook. started arizona jurisdiction with SoS

--- az/__init__.py
+++ az/__init__.py
@@ -17,4 +17,29 @@ class Arizona(Jurisdiction):
     }
 
     def get_organizations(self):
-        yield Organization(name=None, classification=None)
+
+        # Initialize the Organization class. Use keyword args to set the basic
+        # properties.
+        secretary_of_state = Organization(
+            name="Office of the Secretary of State, State of Arizona",
+            classification="office"
+        )
+
+        #  
+        secretary_of_state.add_contact_detail(
+            type="voice",
+            value="602-542-4285"
+        )
+
+        secretary_of_state.add_contact_detail(
+            type="address",
+            value="1700 W Washington St Fl 7, Phoenix AZ 85007-2808"
+        )
+        secretary_of_state.add_link(
+            url="http://www.azsos.gov/",
+            note="Home page"
+        )
+
+        self._secretary_of_state = secretary_of_state
+
+        yield secretary_of_state
