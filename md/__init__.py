6ab068f58e32bc578cc379c78c9f1814cb9f0bca
Benjamin Atwood <batwood@PC101561.local>
2015-04-13 11:19:50 -0400

    start Maryland scraper

---
+++ md/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) Sunlight Foundation, 2014, under the terms of the BSD-3
+# license, a copy of which is in the root level LICENSE file.
+#   Paul R. Tagliamonte <paultag@sunlightfoundation.com>
+
+from pupa.scrape import Jurisdiction, Post, Organization
+from .people import MarylandPersonScraper
+
+
+class Maryland(Jurisdiction):
+    division_id = 'ocd-division/country:us/state:ak'
+    classification = 'government'
+    name = "Maryland State Government"
+    url = 'http://maryland.gov/'
+
+    scrapers = {
+        "people": MarylandPACScraper,
+    }
+
+    def get_organizations(self):
+        org = Organization(name='Maryland Executive Branch',
+                           classification='executive')
+        yield org
