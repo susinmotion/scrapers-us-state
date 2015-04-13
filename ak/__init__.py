90f2e878ffd25e35e1e9c9bc2806bcac18c8343b
Paul Tagliamonte <tag@pault.ag>
2015-04-13 10:37:15 -0400

    Start an init.

---
+++ ak/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) Sunlight Foundation, 2014, under the terms of the BSD-3
+# license, a copy of which is in the root level LICENSE file.
+#   Paul R. Tagliamonte <paultag@sunlightfoundation.com>
+
+from pupa.scrape import Jurisdiction, Post, Organization
+from .people import AlabamaPersonScraper
+
+
+class Alaska(Jurisdiction):
+    division_id = 'ocd-division/country:us/state:ak'
+    classification = 'government'
+    name = "Alaska State Government"
+    url = 'http://alaska.gov/'
+
+    scrapers = {
+        "people": AlaskaPACScraper,
+    }
+
+    def get_organizations(self):
+        org = Organization(name='Alabama Executive Branch',
+                           classification='executive')
+        yield org
