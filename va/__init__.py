56aba4a1a8b636d308a69e95dffc06d0c7b04f75
Tom Baxter <tkb608@gmail.com>
2015-04-15 09:10:02 -0400

    Add Jurisdiction Office of the Secretary of the Commonwealth Add VirginiaDisclosureScraper - first 10 only Move bills.py to people.py

--- va/__init__.py
+++ va/__init__.py
@@ -1,20 +1,40 @@
 # encoding=utf-8
 from pupa.scrape import Jurisdiction, Organization
-from .disclosures import VirginiaDisclosureScraper
-from .bills import VirginiaBillScraper
 from .events import VirginiaEventScraper
+from .people import VirginiaPersonScraper
+from .disclosures import VirginiaDisclosureScraper
 
 
 class Virginia(Jurisdiction):
-    division_id = "va"
+    division_id = "ocd-division/country:us/state:va"
     classification = "government"
     name = "Virginia"
     url = "http://www.virginia.gov/"
     scrapers = {
         "disclosures": VirginiaDisclosureScraper,
-        "bills": VirginiaBillScraper,
         "events": VirginiaEventScraper,
+        "people": VirginiaPersonScraper,
     }
 
     def get_organizations(self):
-        yield Organization(name=None, classification=None)
+
+        secretary_of_the_commonwealth = Organization(
+            name="Office of the Secretary of the Commonwealth, Commonwealth of Virginia",
+            classification="office"
+        )
+        secretary_of_the_commonwealth.add_contact_detail(
+            type="voice",
+            value="804-786-2441"
+        )
+        secretary_of_the_commonwealth.add_contact_detail(
+            type="address",
+            value="1111 East Broad Street, 4th Floor, Richmond, Virginia 23219"
+        )
+        secretary_of_the_commonwealth.add_link(
+            url="https://commonwealth.virginia.gov/",
+            note="Home page"
+        )
+
+        self._secretary_of_the_commonwealth = secretary_of_the_commonwealth
+
+        yield secretary_of_the_commonwealth
